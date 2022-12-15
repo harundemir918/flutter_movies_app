2d2877e0f87729347a10e51b51919767e4e92a29
DESKTOP-55M6QFP\BHD-918 <harun@besport.ist>
2022-12-15 20:14:51 +0300

    code fix

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,12 +1,19 @@
+import 'package:flutter/material.dart';
+
 import '../config/config.dart';
 
 const String appName = 'Movies App';
 const String appAuthorName = 'Harun Demir';
-
-const String appLogo = 'assets/images/logo.png';
 
 const String apiKey = configApiKey;
 const String apiUrl = configApiUrl;
 const String getGenresUrl = configGetGenresUrl;
 const String getMoviesUrl = configGetMoviesUrl;
 const String getMovieDetailUrl = configGetMovieDetailUrl;
+
+const Color kPrimaryColor = Colors.red;
+const Color kBlackColor = Colors.black;
+const Color kWhiteColor = Colors.white;
+const Color kShadowColor = Colors.black12;
+const Color kTransparentColor = Colors.transparent;
+const Color kStarColor = Colors.amber;
